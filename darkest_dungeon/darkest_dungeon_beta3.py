881fd1d649c7e0f771309d0b706a42280927eb5b
Gloaty <95916698+Gloaty@users.noreply.github.com>
2022-02-01 18:58:43 +0000

    Add files via upload

---
+++ darkest_dungeon/darkest_dungeon_beta3.py
@@ -0,0 +1,91 @@
+# imports
+import random
+import time
+
+# current time
+current_time = time.strftime("%H:%M:%S")
+print(current_time)
+sleep = random.choice("0" "1")
+if sleep == "0":
+    time.sleep(1)
+if sleep == "1":
+    time.sleep(2)
+
+# starter heroes
+highwayman = "Dismas"
+crusader = "Reynauld"
+
+# starting gold
+gold = 2500
+
+# welcome
+print("Darkest Dungeon is about making the most of a bad situation. Quests will fail or must be abandoned. ")
+print("Heroes will die. And when they die, they stay dead. Progress autosaves frequently, so actions are")
+print("permanent. This game expects a lot out of you. How far will you push your adventurers? How much are")
+print("you willing to risk in your quest to restore the hamlet? What will you sacrifice to save the life of")
+print("your favorite hero? Thankfully, there are always fresh souls arriving on the stage coach, seeking both")
+print("adventure and fame in the shadow of the...")
+time.sleep(1)
+print(" ")
+print("------------------------------------------------------------------------------------------------------")
+print(" ")
+print("                                     DARKEST DUNGEON")
+print(" ")
+save = input("                                  START SAVE FILE? (Y/N)")
+save.lower()
+if save == "y":
+    print(" ")
+    hamlet_name = input("                                  HAMLET NAME: ")
+    print(" ")
+    center_hamlet_name = ("                                     ""THE " + hamlet_name.upper() + " HAMLET")
+    print(center_hamlet_name)
+    break_counter = 0
+    for i in range(1, 3):
+        print("                                     OPENING SAVE.")
+        time.sleep(0.5)
+        print(" ")
+        print("                                     OPENING SAVE..")
+        time.sleep(0.5)
+        print(" ")
+        print("                                     OPENING SAVE...")
+        time.sleep(0.5)
+        print(" ")
+        break_counter = break_counter + 1
+        if break_counter == 2:
+            break
+
+    # old road
+    print(
+        "Your journey starts on the old road, overrun by raiders, and many supernatural threats. Purge these fools, as a message")
+    print("to their leaders that the rightful owner had returned, and their kind are no longer welcome. ")
+    print(" ")
+    print("----------------------------------------------------------------------------------------------------------------------")
+    print(" ")
+    while True:
+        old_road1 = input("You are currently in a room. This room is empty. Head to the right? (Y/N)")
+        if old_road1 == "Y":
+            print("Reynauld and Dismas push forward, towards the hamlet. They discover an abandoned tent along the way. Investigate? (Y/N)")
+        if old_road1 == "N":
+            tutorial_skip_warning = input("WARNING! THIS WILL SKIP THE TUTORIAL. ARE YOU SURE? WE DO NOT RECOMMEND THIS! (Y/N)")
+            if tutorial_skip_warning == "Y":
+                print("Reynauld and Dismas sit idly waiting for instructions, and are then jumped by monsters. With their ")
+                hamlet_name.lower()
+                print("last words, they urge you to run away. You do, and you reach the " + hamlet_name + " hamlet. ")
+                break
+            if tutorial_skip_warning == "N":
+                print("Good choice. ")
+                print("Resetting... ")
+                time.sleep(1)
+                continue
+else:
+    print("Closing Game...")
+    time.sleep(1)
+    exit()
+
+# dump
+# if save == "n":
+#    print("Closing Game...")
+#    time.sleep(1)
+#    exit()
+#
+#old_road1_bad_directions = ("U", "u", "L", "l", "D", "d")
